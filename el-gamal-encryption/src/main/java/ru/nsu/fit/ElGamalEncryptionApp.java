db23af28b92adc03b89e68fbcb16000a3c2c9bac
Efim Nikitin <e.nikitin1@g.nsu.ru>
2022-09-06 11:22:23 +0700

    Added code for testing encryption from the console

--- el-gamal-encryption/src/main/java/ru/nsu/fit/ElGamalEncryptionApp.java
+++ el-gamal-encryption/src/main/java/ru/nsu/fit/ElGamalEncryptionApp.java
@@ -1,7 +1,38 @@
 package ru.nsu.fit;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ru.nsu.fit.encryption.Ciphertext;
+
+import java.util.Scanner;
+
 public class ElGamalEncryptionApp {
+    private static final Logger logger = LogManager.getLogger(ElGamalEncryptionApp.class);
+
+    private static boolean checkEncryption(int originalMessage) {
+        Client alice = new Client("Alice");
+        Client bob = new Client("Bob");
+
+        Ciphertext aliceCiphertext = alice.encryptMessage(originalMessage, bob.getPublicKey());
+
+        /* Алиса отправляет Бобу шифртекст. Боб получает данную пару */
+
+        Ciphertext bobCiphertext = new Ciphertext(aliceCiphertext);
+
+        int decryptedMessage = bob.decryptMessage(bobCiphertext);
+
+        return originalMessage == decryptedMessage;
+    }
+
     public static void main(String[] args) {
-        /* См. в src.test.java.ru.nsu.fit.ElGamalEncryptionTests */
+        /* Тесты см. в src.test.java.ru.nsu.fit.ElGamalEncryptionTests */
+
+        Scanner scanner = new Scanner(System.in);
+        int message = scanner.nextInt();
+        try {
+            logger.info("Result of encryption: {}", checkEncryption(message));
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
     }
 }
