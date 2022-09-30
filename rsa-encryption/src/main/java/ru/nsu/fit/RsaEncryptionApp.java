2c0d55b618ed497f801b1a630fee1aeb94353f14
Efim Nikitin <e.nikitin1@g.nsu.ru>
2022-09-30 15:24:29 +0700

    Added main class

---
+++ rsa-encryption/src/main/java/ru/nsu/fit/RsaEncryptionApp.java
@@ -0,0 +1,32 @@
+package ru.nsu.fit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Scanner;
+
+public class RsaEncryptionApp {
+    private static final Logger logger = LogManager.getLogger(RsaEncryptionApp.class);
+
+    private static boolean checkEncryption(int originalMessage) {
+        Client alice = new Client("Alice", 131, 227);
+        Client bob = new Client("Bob", 113, 281);
+
+        int encryptedMessage = alice.encryptMessage(originalMessage, bob);
+
+        /* Алиса отправляет Бобу шифртекст. Боб получает данную пару */
+
+        int decryptedMessage = bob.decryptMessage(encryptedMessage);
+
+        return originalMessage == decryptedMessage;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int message = scanner.nextInt();
+            logger.info("Result of encryption: {}", checkEncryption(message));
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
